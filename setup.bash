55e97fec3f97fac18cb8ad76ca0903f719b5e291
Daniel Li <inflmts@gmail.com>
2023-04-02 23:06:23 -0400

    use more galconf

--- setup.bash
+++ setup.bash
@@ -1,7 +1,19 @@
+add_setup_dep .config/gale/config
+
+enable_systemd=$(galconf --get --default=no systemd) || exit
+enable_sway=$(galconf --get --default=no sway) || exit
+enable_hyprland=$(galconf --get --default=no hyprland) || exit
+if [[ $enable_sway = yes ]] || [[ $enable_hyprland = yes ]]; then
+  # if any of these are enabled, wayland is also automatically enabled
+  enable_wayland=yes
+else
+  enable_wayland=$(galconf --get --default=no wayland) || exit
+fi
+
 # internal
-add_symlink .local/bin/galconf .gale/internal/galconf
-add_symlink .local/bin/galsetup .gale/internal/galsetup
-add_symlink .local/bin/galupd .gale/internal/galupd
+add_symlink {.local/bin,.gale/internal}/galconf
+add_symlink {.local/bin,.gale/internal}/galsetup
+add_symlink {.local/bin,.gale/internal}/galupd
 
 # bash
 add_symlink .bashrc .gale/bash/bashrc
@@ -12,18 +24,14 @@ add_symlink .inputrc .gale/bash/inputrc
 add_symlink .vimrc .gale/vim/vimrc
 add_symlink .local/bin/get-vim-plug .gale/vim/get-vim-plug
 
-add -g -o .vim/autoload/plug.vim \
-    -c '.gale/bin/get-vim-plug' \
-    -d 'Downloading vim-plug...'
-
 # lf
-add_symlink .config/lf/lfrc .gale/lf/lfrc
-add_symlink .local/bin/gale-win-lf .gale/lf/gale-win-lf
+add_symlink {.config/lf,.gale/lf}/lfrc
+add_symlink {.local/bin,.gale/lf}/gale-win-lf
 
 # nnn
 add_symlink {.local/bin,.gale/nnn}/n3
 add_symlink {.local/bin,.gale/nnn}/gale-win-n3
-add_symlink .data/applications/gale-win-n3.desktop .gale/nnn/gale-win-n3.desktop
+add_symlink {.data/applications,.gale/nnn}/gale-win-n3.desktop
 
 for plugin in terminal
 do add_symlink ".config/nnn/plugins/$plugin" ".gale/nnn/plugins/$plugin"
@@ -42,20 +50,20 @@ add_symlink .lesskey .gale/less/lesskey
 add_symlink .local/bin/term .gale/bin/term
 add_symlink .local/bin/stupid .gale/bin/stupid
 
-if :; then # wayland
+if [[ $enable_wayland = yes ]]; then
   add_symlink .config/foot/foot.ini .gale/foot/foot.ini
   add_symlink .config/alacritty/alacritty.yml .gale/alacritty/alacritty.yml
   add_symlink .local/bin/wautosleep .gale/wayland/wautosleep
 fi
 
-if :; then # sway
+if [[ $enable_sway = yes ]]; then
   add_symlink .config/sway/config .gale/sway/sway.conf
   add_symlink .local/bin/gale-sway .gale/sway/gale-sway
   add_symlink .local/bin/swaystatus .gale/sway/swaystatus
   add_symlink .local/bin/sway-output-mode .gale/sway/sway-output-mode
 fi
 
-if :; then # hyprland
+if [[ $enable_hyprland = yes ]]; then
   add_symlink .config/hypr/hyprland.conf .gale/hypr/hyprland.conf
   add_symlink .local/bin/gale-hyprland .gale/hypr/gale-hyprland
 fi
