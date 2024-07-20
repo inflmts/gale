fe1c4b3542d37afcf334c823720b85651503df45
Daniel Li <inflmts@gmail.com>
2024-07-20 12:34:11 -0400

    sway: remove range restrictions on empty-workspace

--- sway/empty-workspace.sh
+++ sway/empty-workspace.sh
@@ -2,7 +2,7 @@
 
 set -ue
 ws=$(swaymsg -t get_workspaces \
-  | jq -re '[range(4;11)] - map(select(.focus | length > 0) | .num) | first')
+  | jq -re '[range(1;11)] - map(select((.focus | length > 0) or .visible and (.focused | not)) | .num) | first')
 case $1 in
   focus) swaymsg "workspace number $ws" ;;
   move) swaymsg "move container to workspace number $ws; workspace number $ws" ;;
